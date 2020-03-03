a5ed75ad95663880a35b558f10e05c0cd20eba1d
cassioAndrade <58600993+cassioAndrade@users.noreply.github.com>
2020-03-03 13:26:53 -0500

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,43 @@
+const menu = [
+  {
+    title: 'Menu 1',
+    soup: 'Onion',
+    soupPrice: '1.90',
+    salad: 'Letuce',
+    saladPrice: '3.00'
+   
+  },
+ {
+    title: 'Menu 2',
+    soup: 'Carrot',
+    soupPrice: '2.00',
+    salad: 'Papaya',
+    saladPrice: '3.00'
+    
+  },
+  {
+    title: 'Menu 3',
+    soup: 'garlic',
+    soupPrice: '2.50',
+    salad: 'Mango',
+    saladPrice: '3.25'
+    
+  },
+  {
+    title: 'Menu 4',
+    soup: 'Tomato',
+    soupPrice: '2.25',
+    salad: 'Cucumber',
+    saladPrice: '4.00'
+   
+  },
+  {
+    title: 'Menu 5',
+    soup: 'Potato',
+    soupPrice: '2.50',
+    salad: 'Caesar',
+    saladPrice: '4.15'
+    
+  }];
+
+
