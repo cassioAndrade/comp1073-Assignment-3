c52ada303b3251f5498ec02ff980660dd2033cfb
cassioAndrade <58600993+cassioAndrade@users.noreply.github.com>
2020-03-03 13:19:52 -0500

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,58 @@
+const menu = [
+  {
+    title: 'Menu 1',
+    soup: 'Onion',
+    soupPrice: '1.90',
+    salad: 'Letuce',
+    saladPrice: '3.00',
+    lighterFare: 'Vegetable Biryani',
+    lighterFarePrice: '3.50',
+    entree: 'Breaded Chicken on a Bun',
+    entreePrice: '5.00'
+  },
+ {
+    title: 'Menu 2',
+    soup: 'Carrot',
+    soupPrice: '2.00',
+    salad: 'Julienne',
+    saladPrice: '3.00',
+    lighterFare: 'Madras Chicken Salad Wrap',
+    lighterFarePrice: '4.50',
+    entree: 'Fish & Chips',
+    entreePrice: '6.00'
+  },
+  {
+    title: 'Menu 3',
+    soup: 'Spicy Squash & Pumpkin',
+    soupPrice: '2.50',
+    salad: 'Mediterranean',
+    saladPrice: '3.25',
+    lighterFare: 'Ham & Cheese Panini',
+    lighterFarePrice: '5.25',
+    entree: 'Butter Chicken with Rice Pilaf',
+    entreePrice: '3.25'
+  },
+  {
+    title: 'Menu 4',
+    soup: 'Clam Chowder',
+    soupPrice: '2.25',
+    salad: 'Israeli Couscous & Feta',
+    saladPrice: '4.00',
+    lighterFare: 'Pulled Pork on a Bun',
+    lighterFarePrice: '5.00',
+    entree: 'Matar Paneer & Basmati Rice',
+    entreePrice: '6.00'
+  },
+  {
+    title: 'Menu 5',
+    soup: 'Tomato',
+    soupPrice: '2.50',
+    salad: 'Caesar',
+    saladPrice: '4.15',
+    lighterFare: 'Burger & Fries',
+    lighterFarePrice: '4.00',
+    entree: 'Beef & Broccoli with Fried Rice',
+    entreePrice: '4.95'
+  }];
+
+
